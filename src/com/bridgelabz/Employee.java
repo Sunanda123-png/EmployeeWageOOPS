a70a6bd8153dffe6eb51a11e25afa286f99fcf70
Sunanda123-png <sunanda02@gmail.com>
2021-10-01 01:26:41 +0530

    Adding daily wages of an employee

--- src/com/bridgelabz/Employee.java
+++ src/com/bridgelabz/Employee.java
@@ -1,29 +1,59 @@
 package com.bridgelabz;
 
+/**
+ * author:-Sunanda
+ */
+
 public class Employee {
     private final static int IS_FULL_TIME = 1;
+    private final static int Fullday_hr = 8;
+    private final static int Wages_per_hr=20;
+
+
     /**
      * Purpose - checks whether an Employee is present or absent
+     *
      */
-    public void calculation()
+    public static int calculation()
     {
-        double empCheck = Math.floor(Math.random()*10)%2;
+        int Working_hr_perday;
+        int empCheck = (int)Math.floor(Math.random()*10)%2;
         if (empCheck==IS_FULL_TIME)
         {
             System.out.println("Employee is present");
+            Working_hr_perday=Fullday_hr;
         }
         else
         {
             System.out.println("Employee is not present");
+            Working_hr_perday=0;
+
         }
+        return Working_hr_perday;
     }
-}
-class PresentAbsent
-{
+
+    /**
+     * calculation of daily wages
+     * @param Working_hr_perday
+     * @return
+     */
+    public int daily_employee_wage(int Working_hr_perday)
+    {
+
+        int daily_employee_wage=Wages_per_hr*Working_hr_perday;
+        System.out.println("Daily wages of the employee is:-"+daily_employee_wage);
+        return daily_employee_wage;
+    }
+
+    /**
+     * execution of all method
+     * @param args
+     */
     public static void main(String[] args)
     {
-        Employee e=new Employee();
-        e.calculation();
-
+        Employee employeeWage=new Employee();
+        int Working_hr_perday=employeeWage.calculation();
+        employeeWage.daily_employee_wage(Working_hr_perday);
     }
 }
+
